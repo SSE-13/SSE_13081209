b43313e6b600309c5a48215275b80dcaa4a74292
unknown <magic813>
2016-04-07 16:17:52 +0800

    Finish Homework 6

--- mouseevent/game.ts
+++ mouseevent/game.ts
@@ -1,24 +1,43 @@
 
 var humanContainer = new render.DisplayObjectContainer();
-var head = new render.Bitmap();
-head.x = 100;
-head.source = "wander-icon.jpg";
-humanContainer.addChild(head);
 
+var head = new render.Bitmap();
+var leftarm = new render.Bitmap();
+var rightarm = new render.Bitmap();
+var leftleg = new render.Bitmap();
+var rightleg = new render.Bitmap();
+var Hbody = new render.Bitmap();
+
+head.source = "head.png";
+leftarm.source = "leftarm.png";
+rightarm.source = "rightarm.png";
+leftleg.source = "leftleg.png";
+rightleg.source = "rightleg.png";
+Hbody.source = "body.png";
+
+humanContainer.addChild(head)
+humanContainer.addChild(leftarm)
+humanContainer.addChild(rightarm)
+humanContainer.addChild(leftleg)
+humanContainer.addChild(rightleg)
+humanContainer.addChild(Hbody)
 
 var renderCore = new render.RenderCore();
-renderCore.start(humanContainer, ["wander-icon.jpg"]);
+renderCore.start(humanContainer, ["head.png", "leftarm.png", "rightarm.png", "leftleg.png", "rightleg.png", "body.png"]);
+
+
 
 class HumanBody extends Body {
-    
-    
-    vx:number = 5;
-    
+
+
+    vx: number = 5;
+    V = Math.PI / 2;
 
     onTicker(duringTime: number) {
-        this.x = 100;//+= duringTime * this.vx;
-        this.y = 100;
-
+        body.vx = 5;
+        this.x += this.vx * duringTime;
+        this.y = 200;
+        this.rotation += this.V * duringTime;
     }
 }
 
@@ -30,17 +49,75 @@ ticker.start([body]);
 var eventCore = new events.EventCore();
 eventCore.init();
 
-var headHitTest = (localPoint:math.Point,displayObject:render.DisplayObject) =>{
-    alert (`点击位置为${localPoint.x},${localPoint.y}`);
-    return true;
+var head2 = 0;
+var head3 = false;
+var leg2 = 0;
+var leg3 = false;
+
+var headHitTest = (localPoint: math.Point, displayObject: render.DisplayObject) => {
+
+    if (localPoint.x <= Math.abs(displayObject.x * 6) && localPoint.y <= Math.abs(displayObject.y) && localPoint.x > 0 && localPoint.y > 0) {
+        head2 += 1;
+        head3 = true;
+
+    }
+    return head3;
+
+
+}
+
+var LegHitTest = (localPoint: math.Point, displayObject: render.DisplayObject) => {
+
+    if (localPoint.x > 0 && localPoint.x <= Math.abs(displayObject.x * 2) && localPoint.y > 0 && localPoint.y < Math.abs(displayObject.y * 2)) {
+        leg2 += 1;
+        leg3 = true;
+    }
+
+    return leg3;
+
 }
 
 var headOnClick = () => {
-    alert("clicked!!");
-    //修改 HumanBody 的速度，使其反向移动
+    if (head2 == 1) {
+        if (body.vx != 0) {
+            body.vx *= -1;
+            body.V *= -1;
+        }
+        if (body.vx == 0) {
+            head2 = 0;
+        }
+
+    }
+
+    if (head2 != 1) {
+        body.vx = 5;
+        body.V = Math.PI / 2;
+        head2 = 0;
+    }
+    head3 = false;
 }
 
-eventCore.register(head,headHitTest,headOnClick);
+var LegOnClick = () => {
+
+    if (leg2 == 1) {
+
+        body.vx = 0;
+        body.V = 0;
+        body.rotation = 0;
+    }
+    if (leg2 >= 1) {
+
+        leg2 = 0;
+    }
+    leg3 = false;
+
+
+
+}
+
+eventCore.register(head, headHitTest, headOnClick);
+eventCore.register(leftleg, LegHitTest, LegOnClick);
+eventCore.register(rightleg, LegHitTest, LegOnClick);
 
 
 
